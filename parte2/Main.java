0efa13daf38680e262af4bdbc93895664210603c
guesposar <guesposar@icloud.com>
2024-05-26 18:38:44 -0300

    adição dos cenários de teste

--- parte2/Main.java
+++ parte2/Main.java
@@ -7,7 +7,20 @@ public class Main {
     public static void main(String... args) {
         LoginController login = new LoginController();
 
-        var um = login.cadastrar("spec@email.com", "Spec123sdfasd4@");
-        System.out.println(um);;
+        var loginFalha = login.login("gustavo@example.com", "Gustavo_912345");
+        var loginSucesso = login.login("zoador@example.com", "Zo@dor78912345");
+
+        var cadastroJaExiste = login.cadastrar("fuba@example.com", "Fub@5678912345");
+        var emailFalha = login.cadastrar("fuba@.com", "Fub@5678912345");
+        var senhaFalha = login.cadastrar("fuba@example.com", "Fub@56");
+        var cadastroSucesso = login.cadastrar("gustavo@example.com", "Gustavo@9123456");
+
+        System.out.printf("Cenário de login falha: %b\n", loginFalha);
+        System.out.printf("Cenário de login sucesso: %b\n", loginSucesso);
+        System.out.printf("Cenário de cadastro já existente: %b\n", cadastroJaExiste);
+        System.out.printf("Cenário de email falha: %b\n", emailFalha);
+        System.out.printf("Cenário de senha falha: %b\n", senhaFalha);
+        System.out.printf("Cenário de cadastro sucesso: %b\n", cadastroSucesso);
+        
     }
 }
